81c1dbd1e715d839faa2322b06589fffd9fd8d47
AshokPokharkar <ashokpokharkar@gmail.com>
2016-12-28 15:36:04 -0500

    added in comment
     :: Spring Boot ::  (v1.4.2.RELEASE)

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
- * This is sample application to demo Spring Boot MicroService
+ * This is sample application to demo Spring Boot MicroService using  :: Spring Boot ::        (v1.4.2.RELEASE) 
 **/
 @SpringBootApplication
 public class DemoApplication {
