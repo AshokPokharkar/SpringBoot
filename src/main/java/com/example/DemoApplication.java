12637e010f62343032cac4dbcd137e78a9eb4642
AshokPokharkar <ashokpokharkar@gmail.com>
2016-12-28 15:34:13 -0500

    Ashok - Added comment

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * This is sample application to demo Spring Boot MicroService
+**/
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args); 
 	}
 }
